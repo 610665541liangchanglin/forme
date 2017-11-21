d861297f2e58a3a4d1ee91d90e306289bb1ebcfd
610665541liangchanglin <610665541@qq.com>
2017-11-21 12:12:50 +0800

    个人简历

---
+++ js/circle.js
@@ -0,0 +1,115 @@
+// 圆形进度条
+$(function(){
+
+	$('#html').radialIndicator({
+				radius:70,
+				barColor: '#ff675f',
+				barWidth: 10,
+				initValue: 0,
+				roundCorner : true,
+				percentage: true
+			});
+			var radialObj1 = $('#html').data('radialIndicator');
+			radialObj1.animate(80);
+
+			$('#css').radialIndicator({
+				radius:70,
+				barColor: '#ff675f',
+				barWidth: 10,
+				initValue: 0,
+				roundCorner : true,
+				percentage: true
+			});
+			var radialObj2 = $('#css').data('radialIndicator');
+			radialObj2.animate(80);
+			$('#javascript').radialIndicator({
+				radius:70,
+				barColor: '#ff675f',
+				barWidth: 10,
+				initValue: 0,
+				roundCorner : true,
+				percentage: true
+			});
+			var radialObj3 = $('#javascript').data('radialIndicator');
+			radialObj3.animate(70);
+			$('#jquery').radialIndicator({
+				radius:70,
+				barColor: '#ff675f',
+				barWidth: 10,
+				initValue: 0,
+				roundCorner : true,
+				percentage: true
+			});
+			var radialObj4 = $('#jquery').data('radialIndicator');
+			radialObj4.animate(70);
+			$('#bootstrap').radialIndicator({
+				radius:70,
+				barColor: '#ff675f',
+				barWidth: 10,
+				initValue: 0,
+				roundCorner : true,
+				percentage: true
+			});
+			var radialObj5 = $('#bootstrap').data('radialIndicator');
+			radialObj5.animate(70);
+			$('#java').radialIndicator({
+				radius:70,
+				barColor: '#ff675f',
+				barWidth: 10,
+				initValue: 0,
+				roundCorner : true,
+				percentage: true
+			});
+			var radialObj6 = $('#java').data('radialIndicator');
+			radialObj6.animate(70);
+			$('#ps').radialIndicator({
+				radius:70,
+				barColor: '#ff675f',
+				barWidth: 10,
+				initValue: 0,
+				roundCorner : true,
+				percentage: true
+			});
+			var radialObj7 = $('#ps').data('radialIndicator');
+			radialObj7.animate(30);
+			$('#jdbc').radialIndicator({
+				radius:70,
+				barColor: '#ff675f',
+				barWidth: 10,
+				initValue: 0,
+				roundCorner : true,
+				percentage: true
+			});
+			var radialObj8 = $('#jdbc').data('radialIndicator');
+			radialObj8.animate(80);
+			$('#spring').radialIndicator({
+				radius:70,
+				barColor: '#ff675f',
+				barWidth: 10,
+				initValue: 0,
+				roundCorner : true,
+				percentage: true
+			});
+			var radialObj8 = $('#spring').data('radialIndicator');
+			radialObj8.animate(70);
+			$('#mybatis').radialIndicator({
+				radius:70,
+				barColor: '#ff675f',
+				barWidth: 10,
+				initValue: 0,
+				roundCorner : true,
+				percentage: true
+			});
+			var radialObj9 = $('#mybatis').data('radialIndicator');
+			radialObj9.animate(50);
+			$('#oracle').radialIndicator({
+				radius:70,
+				barColor: '#ff675f',
+				barWidth: 10,
+				initValue: 0,
+				roundCorner : true,
+				percentage: true
+			});
+			var radialObj10 = $('#oracle').data('radialIndicator');
+			radialObj10.animate(80);
+})
